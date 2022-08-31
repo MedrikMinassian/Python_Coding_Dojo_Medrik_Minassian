6574c238cea333a1e583673a0de092ab5eb2261b
MedrikMinassian <medrik.minassian@gmail.com>
2022-08-30 17:46:33 -0700

    end of day practice

---
+++ helloworld_python.py
@@ -0,0 +1,20 @@
+# 1. TASK: print "Hello World"
+greet="Hello"
+print(greet)
+print( "Hello World" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print( greet, name )	# with a comma
+
+print( greet + " "+ name)	# with a +
+
+# # 3. print "Hello 42!" with the number in a variable
+name1 = '42'
+print( greet, name1+'!' )	# with a comma
+# print( your code here )	# with a +	-- this one should give us an error!
+# # 4. print "I love to eat sushi and pizza." with the foods in variables
+# fave_food1 = "sushi"
+# fave_food2 = "pizza"
+# print( your code here ) # with .format()
+# print( your code here ) # with an f string
+
